1ee32ba4130c2c96ad82ac269584654cb271abe4
sorachung <sorachung.src@gmail.com>
2022-01-10 13:21:48 -0600

    displays employees' specialties

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from "react"
 
 export const EmployeeList = () => {
-    const [employees, assignEmployees] = useState([])
+    const [employees, changeEmployee] = useState([])
+    const [specialtiesText, updateSpecialties] = useState("")
 
     useEffect(
         () => {
             fetch("http://localhost:8088/employees")
                 .then(res => res.json())
-                .then(
-                    (employees) => {
-                         assignEmployees(employees);   
-                    }
-                )
+                .then((data) => {
+                    changeEmployee(data)
+                })
         },
         []
     )
 
+    useEffect(() => {
+        const specialties = employees.map(employee => employee.specialty);
+        updateSpecialties(specialties.join(", "))
+    }, [employees])
+
     return (
         <>
+            <div>
+                Specialties: {specialtiesText}
+            </div>
             {
                 employees.map(
                     (employee) => {
-                        return <p key={`employee--${employee.id}`}>{employee.name}</p>;
+                        return <p key={`employee--${employee.id}`}>{employee.name}</p>
                     }
                 )
             }
