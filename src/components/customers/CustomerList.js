746bb1b654065e54efcd9cbd113054fc85bf6f59
sorachung <sorachung.src@gmail.com>
2022-01-10 13:16:12 -0600

    displays total number of customers

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from "react"
 
 export const CustomerList = () => {
-    const [customers, assignCustomers] = useState([])
+    const [customers, setCustomers] = useState([])
+    const [totalCustomerMessage, updateMessage] = useState("")
 
     useEffect(
         () => {
             fetch("http://localhost:8088/customers")
                 .then(res => res.json())
-                .then(
-                    (customers) => {
-                         assignCustomers(customers);   
-                    }
-                )
+                .then((data) => {
+                    setCustomers(data)
+                })
         },
         []
     )
 
+    useEffect(
+        () => {
+            if (customers.length === 1) {
+                updateMessage("You have 1 customer")
+            } else {
+                updateMessage(`You have ${customers.length} customers`)
+            }
+        },
+        [customers]
+    )
+
     return (
         <>
+            <div>{totalCustomerMessage}</div>
             {
-                customers.map(
-                    (customer) => {
-                        return <p key={`customer--${customer.id}`}>{customer.name}</p>;
+                customers.slice(0,5).map(
+                    (customerObject) => {
+                        return <p key={`customer--${customerObject.id}`}>{customerObject.name}</p>
                     }
                 )
             }
