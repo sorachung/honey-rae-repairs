d481894972e740c3ede09ac510458e34b6d53ba8
sorachung <sorachung.src@gmail.com>
2022-01-11 10:49:35 -0600

    added routing and NavBar

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,19 +1,15 @@
 import React from "react"
-import { CustomerList } from "./customers/CustomerList"
-import { EmployeeList } from "./employees/EmployeeList"
-import { TicketList } from "./serviceTickets/TicketList"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import "./Repairs.css"
 
 
 export const Repairs = () => {
     return (
         <>
+            <NavBar />
             <h1>Honey Rae's Repair Shop</h1>
-            <h2>Customer List</h2>
-            <CustomerList />
-            <h2>Employee List</h2>
-            <EmployeeList />
-            <h2>Service Tickets</h2>
-            <TicketList />
+            <ApplicationViews />
         </>
     )
 }
