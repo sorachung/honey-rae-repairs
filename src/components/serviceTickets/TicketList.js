466afc4170b3331b4c6a013dc0b26a138ff70587
sorachung <sorachung.src@gmail.com>
2022-01-11 13:15:17 -0600

    added styling for emergency tickets

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import "./Tickets.css";
 
 export const TicketList = () => {
     const [tickets, updateTickets] = useState([])
@@ -21,9 +22,9 @@ export const TicketList = () => {
                 <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
             </div>
             {tickets.map(ticket => {
-                return <div key={`ticket--${ticket.id}`}>
-                        <p>{ticket.description}. Submitted by {ticket.customer.name} and assigned to {ticket.employee.name}</p>
-                    </div> 
+                return <p className={`ticket--${ticket.id} ${ticket.emergency ? `emergency` : ``}`}>
+                        {ticket.emergency ? "🚑" : ""} {ticket.description} submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                    </p>
             })
             }
             
