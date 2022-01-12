aec05771d20c26d0957dafe74ff5011d06ad5f3e
sorachung <sorachung.src@gmail.com>
2022-01-12 11:28:09 -0600

    added delete ticket functionality

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -6,28 +6,39 @@ import "./Tickets.css";
 export const TicketList = () => {
     const [tickets, updateTickets] = useState([])
     const history = useHistory();
-    useEffect(
-        () => {
-            fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
+
+    const getEmployees = () => {
+        return fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
                 .then(res => res.json())
                 .then((data) => {
                     updateTickets(data)
                 })
-        },
-        []
+    }
+
+    useEffect(
+        () => {getEmployees()},[]
     )
+
+    const deleteTicket = (id) => {
+        fetch(`http://localhost:8088/serviceTickets/${id}`, {
+            method: "DELETE"
+        }).then( () => getEmployees())
+    }
 
     return (
         <>
             <div>
                 <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
             </div>
-            {tickets.map(ticket => {
-                return <p key={`ticket--${ticket.id}`} className={`ticket--${ticket.id} ${ticket.emergency ? `emergency` : ``}`}>
-                        {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link> submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
-                    </p>
-            })
-            }
+            <div className="tickets__list">
+                {tickets.map(ticket => {
+                    return <p key={`ticket--${ticket.id}`} className={`ticket--${ticket.id} ${ticket.emergency ? `emergency` : ``}`}>
+                            {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link> submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                            <button onClick={() => {deleteTicket(ticket.id)}}>Delete</button>
+                        </p>
+                    })
+                }
+            </div>
             
         </>
     )
